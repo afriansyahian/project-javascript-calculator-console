93d0a24b833e185b11ab2110392a89b89367fa16
afriansyahian <afriansyah_i@rocketmail.com>
2019-09-04 08:50:11 +0700

    new update

---
+++ README.md/index.js
@@ -0,0 +1,15 @@
+const digitA = 100
+const digitB = 200.5
+
+const resultAddition = digitA + digitB
+const resultsubstraction = digitA - digitB
+const ressultMultilication = digitA * digitB
+const resultiDivison = digitA / digitB
+
+console.log(digitA)
+console.log(digitB)
+
+console.log(resultAddition)
+console.log(resultsubstraction)
+console.log(ressultMultilication)
+console.log(resultiDivison)
